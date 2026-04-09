d0f11e548d9f4d26aa3772c326fba264a2006b51
Muhammad Abdullah <58142121+Huricane97@users.noreply.github.com>
2026-04-09 20:01:59 +0500

    Delete src directory

--- src/components/speech/index.ts
+++
@@ -1,3 +0,0 @@
-export { SpeechInput, type SpeechInputProps } from './SpeechInput'
-export { SpeechTextarea, type SpeechTextareaProps } from './SpeechTextarea'
-export { useSpeechRecognition } from '../../hooks/useSpeechRecognition'
